e0ef106eda9708bbcdee6fab4f1b2bd55bd7bb70
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-04-29 21:10:31 +0900

    新規コミット

--- Practice-NotificationCenter/ViewController.swift
+++ Practice-NotificationCenter/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label: UILabel!
+
+    private let changeTextModel = ChangeTextModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLabel()
+        NotificationCenter.default.addObserver(
+            forName: .ChangeTextModelDidChangeText,
+            object: nil,
+            queue: OperationQueue.main,
+            using: { [weak self] _ in
+                self?.updateLabel()
+            }
+        )
     }
 
+    @IBAction private func pressedButton(_ sender: Any) {
+        changeTextModel.changeText()
+    }
 
+    private func setUpLabel() {
+        label.text = changeTextModel.text
+    }
+
+    private func updateLabel() {
+        label.text = changeTextModel.text
+    }
 }
 
+extension Notification.Name {
+    static let ChangeTextModelDidChangeText
+    = Notification.Name("ChangeTextModel.didChangeText")
+}
+
+class ChangeTextModel {
+
+    private(set) var text = "ボタンを押してください"
+
+    func changeText() {
+        text = "ありがとう"
+        notify()
+    }
+
+    private func notify() {
+        NotificationCenter.default.post(
+            name: .ChangeTextModelDidChangeText,
+            object: nil
+        )
+    }
+
+}
